aaf9939eb83bcc03ac02c5c3587048879e5c5887
Rizki SetyaPambudi <juangsyah.putra@bni.co.id>
2025-03-14 01:17:03 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using CodeTestGSI;
+
+var solution = new Solution();
+
+
+// Test case  soal 1
+var result1Q1 = solution.Question1("Titanic");
+var result2Q1 = solution.Question1("Avenger Endgame");
+Console.WriteLine("=============================");
+// Test case  soal 2
+var result1Q2 = solution.Question2(result1Q1);
+var result2Q2 = solution.Question2(result2Q1);
+Console.WriteLine("=============================");
+
+// Test case  soal 3
+var result1Q3 = solution.Question3(result1Q2);
+var result2Q3 = solution.Question3(result2Q2);
+Console.WriteLine("=============================");
+
+// Test case  soal 4
+var result1Q4 = solution.Question4(result1Q3);
+var result2Q4 = solution.Question4(result2Q3);
+Console.WriteLine("=============================");
+
+// Test case  soal 5
+var result1Q5 = solution.Question5(result1Q4);
+var result2Q5 = solution.Question5(result2Q4);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CodeTestGSI
+
+[Soal](https://github.com/rizkisetyap/CodeTestGSI/blob/main/Soal%20Coding%20Test.pdf)
